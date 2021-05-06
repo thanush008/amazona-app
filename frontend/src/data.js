55f262802c7273a2a6f2beb9624116af4389cdfc
unknown <sthanusan35@gmail.com>
2021-05-06 23:25:24 +0930

    share the code

---
+++ frontend/src/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products: [
+        {
+            _id : '1',
+            name : 'Nike Slim Shirt',
+            category : 'shirts',
+            image : './images/p1.jpg',
+            price : 120,
+            brand : 'Nike',
+            rating : 4.5,
+            numReviews : 10,
+            description : 'high quality product',
+
+        },
+        {
+            _id : '2',
+            name : 'Nike Slim Shirt',
+            category : 'shirts',
+            image : './images/p2.jpg',
+            price : 100,
+            brand : 'Nike',
+            rating : 4.5,
+            numReviews : 7,
+            description : 'high quality product',
+
+        },
+        {
+            _id : '3',
+            name : 'Nike Slim Shirt',
+            category : 'shirts',
+            image : './images/p3.jpg',
+            price : 130,
+            brand : 'Nike',
+            rating : 4.5,
+            numReviews : 8,
+            description : 'high quality product',
+
+        },
+        {
+            _id : '4',
+            name : 'Nike Slim pants',
+            category : 'pants',
+            image : './images/p4.jpg',
+            price : 200,
+            brand : 'Nike',
+            rating : 4.5,
+            numReviews : 12,
+            description : 'high quality product',
+
+        },
+        {
+            _id : '5',
+            name : 'Nike Slim pants',
+            category : 'pants',
+            image : './images/p5.jpg',
+            price : 220,
+            brand : 'Nike',
+            rating : 4.5,
+            numReviews : 10,
+            description : 'high quality product',
+
+        },
+        {
+            _id : '6',
+            name : 'Nike Slim pants',
+            category : 'pants',
+            image : './images/p6.jpg',
+            price : 210,
+            brand : 'Nike',
+            rating : 4.5,
+            numReviews : 15,
+            description : 'high quality product',
+
+        },
+    ],
+};
+
+export default data;
